fd89cde0f069bc7fd23f547ae03b5102d4d6f726
shashwat12304 <shashwatsharma12304@gmail.com>
2024-01-14 19:01:39 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        It's my passion to work on projects and gain practical experience in order to expand my knowledge of learning the art of algorithmic excellence 
+        and to develop products and services that create a positive impact on the society, simultaneously striding to take tech advancement to the next level.
+        I focus on leveraging my skills and experience to develop products and services that create an impact and make people's lives easier.
+        Thank you for taking the time to read my profile, and I look forward to exploring opportunities to collaborate and drive innovation together. Let's connect and explore how we can collaborate to drive success!
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,257 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "ML Engineer",
+    icon: web,
+  },
+  {
+    title: "Competitive programmer",
+    icon: mobile,
+  },
+  {
+    title: "Web Developer",
+    icon: backend,
+  },
+  {
+    title: "Data Analyst",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "DevOps Intern",
+    company_name: "Alyssum Global Services",
+    icon: tesla,
+    iconBg: "#FFFFFF",
+    date: "May 2022 - October 2022",
+    points: [
+      "Executed infrastructure deployment using Azure Resource Manager,Cloud Formation, and Terraform templates",
+      "Automated backups, efficiently using function apps, improving operational efficiency",
+      "Implemented Azure Synapse deployment pipeline alerts and Log Analytics for proactive monitoring.",
+    ],
+  },
+  {
+    title: "AI Intern",
+    company_name: "Alyssum Global Services",
+    icon: tesla,
+    iconBg: "#FFFFFF",
+    date: "June 2023 - August 2023",
+    points: [
+      "Applied abstractive and extractive text summarization techniques through Transfer Learning",
+      "Collaborated on Discovery AI projects, contributing to enhanced data analysis and information retrieval.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Actively participated in product development, analysis and documentation",
+    ],
+  },
+  {
+    title: "AI intern",
+    company_name: "Juggle",
+    icon: starbucks,
+    iconBg: "#000000",
+    date: "Jan 2024 - Present",
+    points: [
+      "Building production-ready end-to-end AI application with 10,000 initial customer base.",
+      "Designing the application architecture and working on image recognition-based systems and AI assistant systems forthe application.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Whether working independently or collaboratively, Shashwat contributes positively to the work environment, demonstrating adaptability and a commitment to excellence.",
+    name: "Avi Bhardwaj",
+    designation: "Team Lead",
+    company: "Alyssum Global Services",
+    image: "https://raw.githubusercontent.com/shashwat12304/3DPortfolio/main/Avi%20Bhardwaj.png",
+  },
+  {
+    testimonial:
+      "Shashwat is very dedicated and I always get fascinated seeing his work. His problem-solving abilities and attention to detail were invaluable in handling complex tasks and projects. He can effectively work independently and as part of a team, contributing to a positive and productive work environment.",
+    name: "Aditi Jain",
+    designation: "DevOps Engineer",
+    company: "Alyssum Global Services",
+    image: "https://raw.githubusercontent.com/shashwat12304/3DPortfolio/main/Aditi%20Jain.jpg",
+  },
+  {
+    testimonial:
+      "Shashwat is one of the best engineer that I had pleasure to work with. He is talented, punctual and hardworking. We can always expect work delivery before the deadline.",
+    name: "Rishabh Jain",
+    designation: "DevOps Engineer",
+    company: "Alyssum Global Services",
+    image: "https://raw.githubusercontent.com/shashwat12304/3DPortfolio/main/rishabh%20Jain.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "DAKSH",
+    description:
+      "Cloud-based Federated Learning System for Phishing Detection. Tech stack used for the project include TensorFlow, AWS CLI, S3 buckets, EC2 instances,Federated Learning, NFS, Cron Jobs,pickle files, RNN-GRU model, AWS RDS instance, HTML, CSS and Google DrivetoWeb",
+    tags: [
+      {
+        name: "Tensorflow",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Federated Learning",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Amazon Web Services (AWS)",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "GANETIQUE",
+    description:
+      "Created a CycleGAN model capable of performing Style Transfer. The Generative AI model can take any image input can create Monet Style oil paintings from it. Tech Stack used include TensorFlow, Generative Adversarial Networks (GANs) and Style Transfer",
+    tags: [
+      {
+        name: "Gnenerative AI",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Style Transfer",
+        color: "green-text-gradient",
+      },
+      {
+        name: "CycleGANs",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Tensorflow",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Rock VS Mine prediction for Submarines",
+    description:
+      "Developed a Logistic Regression model capable of object identification in submarine environments, bolstering predictive analysis capabilities of a submarine and acting as a defence system.",
+    tags: [
+      {
+        name: "Pandas",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "NumPy",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Logistic Regression",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Data Analysis",
+        color: "green-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
